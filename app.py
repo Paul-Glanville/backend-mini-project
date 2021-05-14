f45828987933d0aa0f60d5080940df81b24db58f
Paul-Glanville <glanville49@googlemail.com>
2021-05-14 19:23:02 +0000

    Add search Functionality

--- app.py
+++ app.py
@@ -25,6 +25,13 @@ def get_tasks():
     return render_template("tasks.html", tasks=tasks)
 
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    tasks = list(mongo.db.tasks.find({"$text": {"$search": query}}))
+    return render_template("tasks.html", tasks=tasks)
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -106,7 +113,7 @@ def add_task():
             "task_name": request.form.get("task_name"),
             "task_description": request.form.get("task_description"),
             "is_urgent": is_urgent,
-            "due_date": request.form.get("due_date")
+            "due_date": request.form.get("due_date"),
             "created_by": session["user"]
         }
         mongo.db.tasks.insert_one(task)
